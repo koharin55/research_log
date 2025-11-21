780d3c51053b5636e666b1bcdfda70cf51c45d34
koharin55 <ysugita0418@gmail.com>
2025-11-22 00:07:42 +0900

    Userモデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :logs,       dependent: :destroy
+  has_many :categories, dependent: :destroy
+
+  validates :nickname, presence: true, length: { maximum: 30 }
 end
