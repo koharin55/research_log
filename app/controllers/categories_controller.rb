329338c046fdbccda68ea04f20627aea472bac11
koharin55 <ysugita0418@gmail.com>
2025-11-23 01:14:00 +0900

    カテゴリ管理画面の作成

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: %i[edit update destroy]
+
+  def index
+    @categories = current_user.categories.order(:position, :name)
+    @category   = current_user.categories.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "カテゴリを作成しました。"
+    else
+      @categories = current_user.categories.order(:position, :name)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "カテゴリを更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "\"#{@category.name}\"カテゴリを削除しました。（関連ログのカテゴリは未設定になります）"
+  end
+
+  private
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :color, :icon, :position)
+  end
+end
