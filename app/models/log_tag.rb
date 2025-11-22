2214379413eda940c627e8483b88236da7db5b67
koharin55 <ysugita0418@gmail.com>
2025-11-22 21:36:01 +0900

    Annotateを実行

--- app/models/log_tag.rb
+++ app/models/log_tag.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: log_tags
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  log_id     :bigint           not null
+#  tag_id     :bigint           not null
+#
+# Indexes
+#
+#  index_log_tags_on_log_id             (log_id)
+#  index_log_tags_on_log_id_and_tag_id  (log_id,tag_id) UNIQUE
+#  index_log_tags_on_tag_id             (tag_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (log_id => logs.id)
+#  fk_rails_...  (tag_id => tags.id)
+#
 class LogTag < ApplicationRecord
   belongs_to :log
   belongs_to :tag
--- spec/factories/logs.rb
+++ spec/factories/logs.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  id          :bigint           not null, primary key
+#  body        :text(65535)
+#  code        :text(65535)
+#  copy_count  :integer          default(0), not null
+#  pinned      :boolean          default(FALSE), not null
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_logs_on_category_id  (category_id)
+#  index_logs_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :log do
     
