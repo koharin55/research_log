89b78aa0977e13c6af95cacbaa757b68a03a23a9
koharin55 <ysugita0418@gmail.com>
2025-12-02 23:32:42 +0900

    カテゴリ必須化に伴うバリデーション変更

--- app/models/log.rb
+++ app/models/log.rb
@@ -37,7 +37,6 @@ class Log < ApplicationRecord
   has_rich_text :memo
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :category_id, numericality: { only_integer: true }, presence: true
   validates :memo,  length: { maximum: 200 }, allow_blank: true
   validate :images_count_within_limit
 
